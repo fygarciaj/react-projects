9cb3cfd8f2c7a6928921bd44fe1b4c6f434daef8
Fabian Garcia <fygarciaj@gmail.com>
2024-05-16 21:02:35 -0500

    db conection datosips

---
+++ paideia-full-next/src/libs/db/dbkibox_datosips.ts
@@ -0,0 +1,11 @@
+import { createPool } from 'mysql2/promise'
+
+const poolDatosIps = createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  port: 3306,
+  database: 'kibox_datosips'
+})
+
+export { poolDatosIps }
---
+++ paideia-full-next/src/prisma/migrations/20240517010933_/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Appointment` (
+    `id` VARCHAR(191) NOT NULL,
+    `dateAppointment` DATETIME(3) NOT NULL,
+    `documentNumber` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
