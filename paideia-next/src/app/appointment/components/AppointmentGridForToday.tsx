d5bd2ed250c67d2f66171dcdbec7469ee5d3ce6b
Fabian Yesith Garcia Jurado <fygarciaj@gmail.com>
2024-05-21 16:44:36 -0500

    DataGrid for today

--- paideia-next/src/app/appointment/components/AppointmentGridForToday.tsx
+++ paideia-next/src/app/appointment/components/AppointmentGridForToday.tsx
@@ -1,13 +1,29 @@
+import { GridColDef, DataGrid } from "@mui/x-data-grid";
 
 export function AppointmentGridForToday({data}) {
 
+  const columns : GridColDef[] = [
+    {field: 'Id', headerName: 'Id', hideable: true},
+    {field: 'Fecha', headerName: 'Fecha', type: 'string', width: 130},
+    {field: 'Hora', headerName: 'Hora'},
+    {field: 'Paciente', headerName: 'Paciente'},
+    {field: 'Entidad', headerName: 'Entidad'},
+    {field: 'Telefono', headerName: 'Contacto'},
+    {field: 'Estado', headerName: 'Estado', },
+  ];
 
-
-
+  console.log(data);
 
   return (
-    <>
-      
-    </>
+      <DataGrid 
+      rows={data}
+      columns={columns} 
+      getRowId={(row) => row.Id}
+      autosizeOptions={{
+        columns: ['Fecha', 'Paciente', 'Estado'],
+        includeOutliers: true,
+        includeHeaders: true,
+      }}
+      />
   );
 }
